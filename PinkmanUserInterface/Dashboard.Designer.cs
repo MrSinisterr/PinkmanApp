e0109fd6d8462295f30c1553c46d6c1b64185ed5
Developer VS-Systems <developer-systems@accesa.eu>
2024-10-21 16:37:50 +0300

    Initial project tweeking.
    - Done a bit of setting up.

--- PinkmanUserInterface/Dashboard.Designer.cs
+++ PinkmanUserInterface/Dashboard.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PinkmanUserInterface
 {
-    partial class Form1
+    partial class Dashboard
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(1867, 1004);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.Name = "Dashboard";
+            this.Text = "Pinkman";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
